06304a7a7c3467976c4c702efa3b012024c97a0c
Ramrachai <ramrachaim@gmail.com>
2020-05-01 20:08:59 +0600

    uploading files

---
+++ a.js
@@ -0,0 +1,23 @@
+let leftList = document.querySelectorAll(".leftList li");
+leftList.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    document.querySelector("#mylist").appendChild(item);
+  });
+});
+
+let rightList = document.querySelector("#mylist");
+let observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length) {
+      rightItems = document.querySelectorAll("#mylist li");
+      rightItems.forEach((item) => {
+        item.addEventListener("click", () => {
+          document.querySelector(".leftList").appendChild(item);
+        });
+      });
+    }
+  });
+});
+observer.observe(rightList, { childList: true });
+
+function mute() {}
